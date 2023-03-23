20897eb8a92b680140dbdbdab5df6dad1666fbe6
ManPanchani <manpanchani0511@gmail.com>
2023-03-23 12:41:08 +0530

    Author App

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:author_app/screens/home_page.dart';
+import 'package:author_app/screens/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        // '/': (context) => const SplashPage(),
+        'HomePage': (context) => const HomePage(),
+        '/': (context) => const LoginPage(),
+      },
+    ),
+  );
+}
